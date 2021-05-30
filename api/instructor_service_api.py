79253c1d16205949eaa8713d5ef4e6260f9cb2d4
bzy <823324993@qq.com>
2021-05-30 13:12:24 +0800

    update methods in instructor_service_api

--- api/instructor_service_api.py
+++ api/instructor_service_api.py
@@ -1,4 +1,4 @@
-from exception import IntegrityViolationError
+from exception import EntityNotFoundError, IntegrityViolationError
 import asyncpg
 from service.instructor_service import InstructorService
 from typing import List
@@ -19,22 +19,24 @@ class instructor_service(InstructorService):
             full_name = last_name+first_name
         async with self.__pool.acquire() as con:
             try:
-                return await con.fetchval('''
+                return await con.execute('''
                 insert into instructor (id, full_name) values (%d, '%s')
                 ''' % (user_id, full_name))
             except asyncpg.exceptions.IntegrityConstraintViolationError as e:
                 raise IntegrityViolationError from e
 
-    # TODO: optimize and index
+    # TODO: index
     async def get_instructed_course_sections(self, instructor_id: int,
                                              semester_id: int
                                              ) -> List[CourseSection]:
         async with self.__pool.acquire() as con:
             res = await con.fetch('''
             select section.id, section.name, total_capacity, left_capacity
-            from instructor join class on instructor.id = class.instructor
-                join section on class.section = section.id
-            where instructor.id = %d and semester = %d
+            from class join section on class.section = section.id
+            where class.instructor = %d and semester = %d
             ''' % (instructor_id, semester_id))
-            return [CourseSection(r['id'], r['name'],
-                    r['total_capacity'], r['left_capacity']) for r in res]
+            if res:
+                return [CourseSection(r['id'], r['name'],
+                        r['total_capacity'], r['left_capacity']) for r in res]
+            else:
+                raise EntityNotFoundError
