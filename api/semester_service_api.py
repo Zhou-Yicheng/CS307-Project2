4fece8c73d20b2526d9a662a0baa78c41a53442d
bzy <823324993@qq.com>
2021-05-30 13:42:10 +0800

    update semester_service_api

--- api/semester_service_api.py
+++ api/semester_service_api.py
@@ -16,7 +16,7 @@ class semester_service(SemesterService):
         async with self.__pool.acquire() as con:
             try:
                 return await con.fetchval('''
-                insert into semester (name,begin_date,end_date)
+                insert into semester (name, begin_date, end_date)
                 values ('%s', '%s', '%s')
                     returning id
                 ''' % (name, begin, end))
@@ -33,15 +33,26 @@ class semester_service(SemesterService):
     async def get_all_semesters(self) -> List[Semester]:
         async with self.__pool.acquire() as con:
             res = await con.fetch('select * from semester')
-            return [Semester(r['id'], r['name'], r['begin_date'], r['end_date']
-                             ) for r in res]
+            if res:
+                return [Semester(
+                                r['id'],
+                                r['name'],
+                                r['begin_date'],
+                                r['end_date']
+                                ) for r in res]
+            else:
+                raise EntityNotFoundError
 
     async def get_semester(self, semester_id: int) -> Semester:
         async with self.__pool.acquire() as con:
             res = await con.fetchrow('select * from semester where id ='
                                      + semester_id)
             if res:
-                return Semester(res['id'], res['name'],
-                                res['begin_date'], res['end_date'])
+                return Semester(
+                                res['id'],
+                                res['name'],
+                                res['begin_date'],
+                                res['end_date']
+                                )
             else:
                 return EntityNotFoundError
