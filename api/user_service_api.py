e64787cd16e696b98ccec120b2e412226ebbeba2
bzy <823324993@qq.com>
2021-06-09 22:30:06 +0800

    update constructor of Student

--- api/user_service_api.py
+++ api/user_service_api.py
@@ -3,7 +3,7 @@ import asyncpg
 from service.user_service import UserService
 from typing import List
 
-from dto import Instructor, Student, User
+from dto import Instructor, Student, User, Major, Department
 
 
 class user_service(UserService):
@@ -21,23 +21,67 @@ class user_service(UserService):
     async def get_all_users(self) -> List[User]:
         async with self.__pool.acquire() as con:
             instructors = await con.fetch('select * from instructor')
-            students = await con.fetch('select * from student')
+            students = await con.fetch('''
+                select student.id as student_id, full_name, enrolled_date,
+                    major.id as major_id, major.name as major_name,
+                    department.id as department_id, department.name as department_name
+                from student join major
+                    on student.major = major.id
+                    join department
+                    on major.department = department.id
+            ''')
             if(instructors and students):
                 return ([Instructor(i['id'], i['full_name']) for i in instructors]
-                        + [Student(s['id'], s['full_name']) for s in students])
+                        + [Student(s['student_id'],
+                                   s['full_name'],
+                                   s['enrolled_date'],
+                                   Major(s['major_id'],
+                                         s['major_name'],
+                                         Department(s['department_id'],
+                                                    s['department_name']
+                                                   )
+                                        )
+                                    ) for s in students])
             elif(instructors):
                 return [Instructor(i['id'], i['full_name']) for i in instructors]
             elif(students):
-                return [Student(s['id'], s['full_name']) for s in students]
+                return [Student(s['student_id'],
+                                s['full_name'],
+                                s['enrolled_date'],
+                                Major(s['major_id'],
+                                      s['major_name'],
+                                      Department(s['department_id'],
+                                                 s['department_name']
+                                                )
+                                     )
+                                ) for s in students]
             else:
                 return []
 
     async def get_user(self, user_id: int) -> User:
         async with self.__pool.acquire() as con:
-            s = await con.fetchrow('select * from student where id='+user_id)
+            s = await con.fetchrow('''
+                select student.id as student_id, full_name, enrolled_date,
+                    major.id as major_id, major.name as major_name,
+                    department.id as department_id, department.name as department_name
+                from student join major
+                    on student.major = major.id
+                    join department
+                    on major.department = department.id
+                where student.id =
+            '''+user_id)
             i = await con.fetchrow('select * from instructor where id='+user_id)
             if s:
-                return Student(s['id'], s['full_name'])
+                return Student(s['student_id'],
+                               s['full_name'],
+                               s['enrolled_date'],
+                               Major(s['major_id'],
+                                     s['major_name'],
+                                     Department(s['department_id'],
+                                                s['department_name']
+                                                )
+                                    )
+                               )
             elif i:
                 return Instructor(i['id'], i['full_name'])
             else:
